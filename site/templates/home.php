9ef6d2003549d9980124ffed116f98a546f25228
Kyle Decker <kylebdecker@gmail.com>
2017-12-22 12:43:46 -0500

    Responsive gallery sizing, filter adjustments

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,9 +1,14 @@
 <?php snippet('nav') ?>
 
-<div class="page-title">
-  <h1>Right Angle Woodworks</h1>
-  <div class="filter-bar filter-button-group">
-    <button data-filter="*" class="is-checked">Show All</button><button data-filter=".furniture">Furniture</button><button data-filter=".remodel">Remodeling</button><button data-filter=".testimonial">Testimonials</button>
+<div class="wrapper">
+  <div class="page-title">
+    <h1>Right Angle Woodworks</h1>
+  </div>
+
+  <div class="gallery-controls">
+    <div class="filter-bar filter-button-group">
+      <button data-filter="*" class="is-checked">Show All</button><button data-filter=".furniture">Furniture</button><button data-filter=".remodel">Remodeling</button><button data-filter=".testimonial">Testimonials</button>
+    </div>
   </div>
 </div>
 
